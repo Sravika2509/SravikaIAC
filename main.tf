3edb2f836d76f6a09ebd6d12316048683ab1dd7a
Sravika <Sravika.Paluru@halliburton.com>
2023-08-31 22:52:04 +0530

    iam role

--- main.tf
+++ main.tf
@@ -1,11 +1,70 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-west-2"
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
+variable "instance_profile_name" {
+  type    = string
+  default = "example-instance-profile"
+}
+
+variable "iam_policy_name" {
+  type    = string
+  default = "example-policy"
+}
+
+variable "role_name" {
+  type    = string
+  default = "example-role"
+}
+
+# Create an IAM policy
+resource "aws_iam_policy" "jenkins_iam_policy" {
+  name = var.iam_policy_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "ssm:GetParameter",
+          "ssm:GetParameters",
+          "ssm:GetParametersByPath"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Create an IAM role
+resource "aws_iam_role" "jenkins_role" {
+  name = var.role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach the IAM policy to the IAM role
+resource "aws_iam_policy_attachment" "jenkins_role_policy_attachment" {
+  name = "Policy Attachement"
+  policy_arn = aws_iam_policy.jenkins_iam_policy.arn
+  roles       = [aws_iam_role.jenkins_role.name]
+}
+
+# Create an IAM instance profile
+resource "aws_iam_instance_profile" "jenkins_instance_profile" {
+  name = var.instance_profile_name
+  role = aws_iam_role.jenkins_role.name
 }
