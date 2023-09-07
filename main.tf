7086001ecbdfc046a8e1d8398a90db0c7ef92956
Sravika <Sravika.Paluru@halliburton.com>
2023-09-07 14:15:41 +0530

    terraform3

--- main.tf
+++ main.tf
@@ -42,24 +42,6 @@ output "public_ips" {
   value = aws_instance.my_instances[*].public_ip
 }
 
-output "ssh_private_key" {
-  value = file("~/.ssh/id_rsa")  # Update this path to your SSH private key file
-}
 
 
-data "aws_key_pair" "my_key_pair" {
-  key_name = "id_rsa"  # Replace with your key pair name
-}
 
-resource "null_resource" "save_key" {
-  triggers = {
-    key_fingerprint = data.aws_key_pair.my_key_pair.key_fingerprint
-  }
-
-  provisioner "local-exec" {
-    command = <<-EOT
-      echo '${data.aws_key_pair.my_key_pair.private_key}' > ~/.ssh/id_rsa.pem
-      chmod 400 ~/.ssh/id_rsa.pem
-    EOT
-  }
-}
